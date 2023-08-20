8e0750edac369b88e09dba25b4a5a2823b92105a
Aaron He <aaronhe09@yahoo.com>
2023-08-20 12:27:15 -0700

    added AnimatePresence to show exit animation when component is unmounted

--- rizz-o-meter/app/page.js
+++ rizz-o-meter/app/page.js
@@ -2,9 +2,12 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import question from "../public/assets/question.json";
 import { useRouter } from "next/navigation";
-import { easeOut, motion } from "framer-motion";
+import { easeOut, motion, AnimatePresence } from "framer-motion";
+import uuid from "react-uuid";
+import { useState } from "react";
 
 export default function Home() {
+  const [isShowing, setIsShowing] = useState(true);
   const router = useRouter();
 
   const variant = {
@@ -22,47 +25,61 @@ export default function Home() {
         easeOut,
       },
     },
+    exit: { opacity: 0 },
   };
 
   const handleClick = () => {
-    router.push("/game");
+    setIsShowing(false);
+
+    setTimeout(() => {
+      router.push("/game");
+    }, 300);
   };
 
   return (
-    <motion.section
-      className="text-center flex flex-col justify-center items-center"
-      variants={variant}
-      initial="hidden"
-      animate="show"
-    >
-      <motion.h1
-        className="text-5xl md:text-6xl font-extrabold leading-[1.15] text-black cursor-default"
-        variants={items}
-      >
-        Welcome to <br className="sm:hidden" />
-        Rizz-O-Meter
-        <br />
-        <motion.span className="orange_gradient inline-block mt-2">
-          Unleash Your <br className="sm:hidden" />
-          Inner "Rizz"!
-        </motion.span>
-      </motion.h1>
-      <Player src={question} autoplay loop className=" w-60"></Player>
-      <motion.p
-        className="text-center text-md text-lg cursor-default max-w-[242px] sm:max-w-none"
-        variants={items}
-      >
-        You're about to embark on a playful journey to determine your "Rizz"
-        level.
-      </motion.p>
-      <motion.button
-        type="button"
-        className=" bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 mt-7 rounded-lg text-2xl border-none py-2 w-52"
-        onClick={handleClick}
-        variants={items}
-      >
-        Get Started
-      </motion.button>
-    </motion.section>
+    <AnimatePresence>
+      {isShowing && (
+        <motion.section
+          className="text-center flex flex-col justify-center items-center"
+          variants={variant}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          key={uuid()}
+        >
+          <motion.h1
+            className="text-5xl md:text-6xl font-extrabold leading-[1.15] text-black cursor-default"
+            variants={items}
+            key={uuid()}
+          >
+            Welcome to <br className="sm:hidden" />
+            Rizz-O-Meter
+            <br />
+            <motion.span className="orange_gradient inline-block mt-2">
+              Unleash Your <br className="sm:hidden" />
+              Inner "Rizz"!
+            </motion.span>
+          </motion.h1>
+          <Player src={question} autoplay loop className=" w-60"></Player>
+          <motion.p
+            className="text-center text-md text-lg cursor-default max-w-[242px] sm:max-w-none"
+            variants={items}
+            key={uuid()}
+          >
+            You're about to embark on a playful journey to determine your "Rizz"
+            level.
+          </motion.p>
+          <motion.button
+            type="button"
+            className=" bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 mt-7 rounded-lg text-2xl border-none py-2 w-52"
+            onClick={handleClick}
+            variants={items}
+            key={uuid()}
+          >
+            Get Started
+          </motion.button>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 }
--- rizz-o-meter/components/Footer.js
+++ rizz-o-meter/components/Footer.js
@@ -1,38 +1,43 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { motion, easeOut } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import uuid from "react-uuid";
 
 const Footer = () => {
   const variant = {
     hidden: { opacity: 0 },
     show: { opacity: 1 },
+    exit: { opacity: 0 },
   };
 
   return (
-    <motion.footer
-      className=" absolute bottom-0 left-1/2 transform -translate-y-1/2 -translate-x-1/2 flex flex-col text-center"
-      variants={variant}
-      initial="hidden"
-      animate="show"
-    >
-      <div>
-        <Link
-          href="https://github.com/AaronHe09"
-          className="font-anonymous text-xl"
-        >
-          GitHub
-        </Link>
-        <span className="font-anonymous text-xl m-2">|</span>
-        <Link
-          href="https://www.linkedin.com/in/aaronhe09/"
-          className="font-anonymous text-xl"
-        >
-          LinkedIn
-        </Link>
-      </div>
-      <p className="font-anonymous text">aaronhe09@yahoo.com</p>
-    </motion.footer>
+    <AnimatePresence>
+      <motion.footer
+        className=" absolute bottom-0 left-1/2 transform -translate-y-1/2 -translate-x-1/2 flex flex-col text-center"
+        variants={variant}
+        initial="hidden"
+        animate="show"
+        key={uuid()}
+      >
+        <div>
+          <Link
+            href="https://github.com/AaronHe09"
+            className="font-anonymous text-xl"
+          >
+            GitHub
+          </Link>
+          <span className="font-anonymous text-xl m-2">|</span>
+          <Link
+            href="https://www.linkedin.com/in/aaronhe09/"
+            className="font-anonymous text-xl"
+          >
+            LinkedIn
+          </Link>
+        </div>
+        <p className="font-anonymous text">aaronhe09@yahoo.com</p>
+      </motion.footer>
+    </AnimatePresence>
   );
 };
 
