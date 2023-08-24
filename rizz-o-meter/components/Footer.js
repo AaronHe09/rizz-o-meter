9fd5cce92925911ac3647b355d7a4733a8aa6a0a
Aaron He <aaronhe09@yahoo.com>
2023-08-24 14:14:06 -0700

    removed height for .app and added z-index so user can hit links on mobile

--- rizz-o-meter/components/Footer.js
+++ rizz-o-meter/components/Footer.js
@@ -13,27 +13,33 @@ const Footer = () => {
   return (
     <AnimatePresence>
       <motion.footer
-        className=" absolute bottom-0 left-1/2 transform -translate-x-1/2 flex flex-col text-center"
+        className=" absolute bottom-0 left-1/2 transform -translate-x-1/2 flex flex-col text-center z-50"
         variants={variant}
         initial="hidden"
         animate="show"
         key={uuid()}
       >
-        <div>
-          <a
-            href="https://github.com/AaronHe09"
-            className="font-anonymous text-xl"
-          >
-            GitHub
-          </a>
-          <span className="font-anonymous text-xl m-2">|</span>
-          <a
-            href="https://www.linkedin.com/in/aaronhe09/"
-            className="font-anonymous text-xl"
-          >
-            LinkedIn
-          </a>
-        </div>
+        <ul className="flex">
+          <li>
+            <a
+              href="https://github.com/AaronHe09"
+              className="font-anonymous text-xl"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <span className="font-anonymous text-xl mx-2">|</span>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/aaronhe09/"
+              className="font-anonymous text-xl"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
         <p className="font-anonymous text">aaronhe09@yahoo.com</p>
       </motion.footer>
     </AnimatePresence>
